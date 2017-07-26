8b72bb810c30bdb974245e5e2a56f2fbc6660af0
Jeannette <jeannette@tastapod.com>
2017-07-26 16:31:01 +0100

    add all weather type backgrounds

--- js/weather.js
+++ js/weather.js
@@ -9,6 +9,43 @@ if (navigator.geolocation) {
       document.getElementById("body").style.backgroundImage = "url(" + image + ")";
     }
 
+    function showWeather(weather) {
+      switch (weather) {
+        case "rain":
+          updateBackground("/img/rain.jpg");
+          break;
+        case "cloudy":
+          updateBackground("/img/cloudy.jpg");
+          break;
+        case "clear-day":
+          updateBackground("/img/clear-day.jpg");
+          break;
+        case "clear-night":
+          updateBackground("/img/clear-night.jpg");
+          break;
+        case "snow":
+          updateBackground("/img/snow.jpg");
+          break;
+        case "sleet":
+          updateBackground("/img/rain.jpg");
+          break;
+        case "wind":
+          updateBackground("/img/wind.jpg");
+          break;
+        case "fog":
+          updateBackground("/img/fog.jpg");
+          break;
+        case "partly-cloudy-day":
+          updateBackground("/img/partly-cloudy-day.jpg");
+          break;
+        case "partly-cloudy-night":
+          updateBackground("/img/partly-cloudy-night.jpg");
+          break;
+        default:
+          updateBackground("/img/default.jpg");
+      }
+    }
+
     function buildWeatherUrl(coords) {
       return "https://crossorigin.me/https://api.darksky.net/forecast/4924a4b17c6e9ad716b4b67b4c27b67f/"
         + coords.latitude
@@ -23,26 +60,31 @@ if (navigator.geolocation) {
 
   		temp = parseInt(json.currently.temperature);
       $(".temperature").html(temp);
-
+      $("#cel").html("&deg;C");
+      $("#far").html("&deg;F");
       $(".city").html(json.timezone);
-      $(".type").html(json.currently.icon);
-      if (json.currently.icon == "cloudy") {
+      $(".weather_type").html(json.currently.icon);
+      showWeather(json.currently.icon)
+      /*if (json.currently.icon == "rain") {
         updateBackground("/img/clouds.jpg");
-      } else if (json.currently.icon == "rain") {
+      } else if (json.currently.icon == "cloudy") {
         updateBackground("/img/rain.jpg");
       } else {
         updateBackground("/img/default.jpg");
-      }
+      }*/
     });
   });
-  $("#unit").on("click", function() {
-  if ($("#unit").html() == "&deg;C") {
-      $("#unit").html("&deg;F");
-      var fahrenheit = parseInt(temp) * 9/5 + 32;
-          $(".temperature").html(fahrenheit);
-  } else {
-    $("#unit").html("&deg;C");
+  function showCelsius() {
     $(".temperature").html(temp);
+    document.getElementById("far").style.fontSize = "small";
+    document.getElementById("cel").style.fontSize = "large";
+
   }
-  });
+  function showFahrenheit() {
+    var fahrenheit = parseInt(temp) * 9/5 + 32;
+        $(".temperature").html(fahrenheit);
+        document.getElementById("far").style.fontSize = "large";
+        document.getElementById("cel").style.fontSize = "small";
+
+  }
 }
